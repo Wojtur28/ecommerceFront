93c6af45742b98eb413d0ec883405801fd052983
Wojturski Maciej <w65578@student.wsiz.edu.pl>
2023-04-07 15:42:22 +0200

    add product category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Product } from "../common/product";
 import { map } from "rxjs/operators";
+import {ProductCategory} from "../common/product-category";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,8 @@ export class ProductService {
 
   private baseUrl = 'http://localhost:8080/products';
 
+  private categoryUrl = 'http://localhost:8080/product-category';
+
   constructor(private httpClient: HttpClient) {
   }
 
@@ -18,14 +21,25 @@ export class ProductService {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products));
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory));
+  }
+
 }
 
-  interface GetResponse {
+  interface GetResponseProducts {
   _embedded: {
     products: Product[];
     }
   }
 
+  interface GetResponseProductCategory {
+    _embedded: {
+      productCategory: ProductCategory[];
+    }
+  }
